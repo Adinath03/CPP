49ddfcd057c03e12bf31030a53ad0684aa7cf92d
Adinath Mahandule <101441112+Adinath03@users.noreply.github.com>
2022-09-27 21:41:12 +0530

    Initial v1

---
+++ Codes/Arrays/Number of occurrence.cpp
@@ -0,0 +1,97 @@
+/*
+Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.  
+*/
+
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int first(int nums[],int size, int target)
+    {
+        int end = size-1;
+        int start = 0,
+        first = -1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(target == nums[mid])
+            {
+                first = mid;
+                end = mid-1;
+            }
+            else if( target < nums[mid])
+            {
+                end = mid-1;
+            }
+            else if(target > nums[mid])
+            {
+                start = mid+1;
+            }
+        }
+        return first;
+    };
+    
+    int last(int nums[],int size,int target)
+    {
+        int end = size-1;
+        int start = 0,
+        last = -1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(target == nums[mid])
+            {
+                last = mid;
+                start = mid+1;
+            }
+            else if( target < nums[mid])
+            {
+                end = mid-1;
+            }
+            else if(target > nums[mid])
+            {
+                start = mid+1;
+            }
+        }
+        return last;
+    };
+	int count(int arr[], int n, int x) {
+	   int initial = first(arr,n,x);
+	   int final = last(arr,n,x);
+	   if(initial == -1)
+	   {
+	       return 0;
+	   }
+	   return (final - initial + 1);
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
